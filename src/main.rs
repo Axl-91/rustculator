754d9cd009a92e55f2f6ea4d5614030c1b08e7a1
Axel Aparicio Rotermund <axelaparicior@gmail.com>
2025-02-09 01:06:54 -0300

    Added reflection

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use tonic::transport::Server;
 
 mod proto {
     tonic::include_proto!("calculator");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("calculator_descriptor");
 }
 
 #[derive(Debug, Default)]
@@ -28,10 +31,16 @@ impl Calculator for CalculatorService {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
+    println!("Listening on: {:?}", addr);
 
     let calc = CalculatorService::default();
 
+    let service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()?;
+
     Server::builder()
+        .add_service(service)
         .add_service(CalculatorServer::new(calc))
         .serve(addr)
         .await?;
